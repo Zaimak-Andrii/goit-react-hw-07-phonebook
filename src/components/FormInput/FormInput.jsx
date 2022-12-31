d404e935952ca33a0bce2b25f362fdf9c47b44b2
Andrii Zaimak <xiveres@MacBook-Air-Andrii.local>
2022-12-31 17:26:18 +0200

    Change Formik to React-hook_form

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -1,18 +1,19 @@
+import { forwardRef } from 'react';
 import {
   StyledErrorMessage,
   StyledFormInput,
   StyledFormLabel,
 } from './FormInput.styled';
 
-export default function FormInput({ children, touched, error, ...props }) {
-  return (
-    <StyledFormLabel>
-      {children}
-      <StyledFormInput {...props} isValid={!error} touched={touched} />
+const FormInput = forwardRef(({ children, touched, error, ...props }, ref) => (
+  <StyledFormLabel>
+    {children}
+    <StyledFormInput {...props} isValid={!error} touched={touched} ref={ref} />
 
-      {touched && error ? (
-        <StyledErrorMessage>{error}</StyledErrorMessage>
-      ) : null}
-    </StyledFormLabel>
-  );
-}
+    {touched && error ? (
+      <StyledErrorMessage>{error.message}</StyledErrorMessage>
+    ) : null}
+  </StyledFormLabel>
+));
+
+export default FormInput;
